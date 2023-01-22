6a366c304a181470f92f321eacc57b00af9c10fb
ionb23 <ion.borziac@gmail.com>
2023-01-22 20:34:41 +0000

    finalised code that creates timeblock and assigns color based on time of the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,76 @@
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM do YYYY"));
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
-var containerEl = $('.container');
+var containerEl = $('#containerEl');
+// var containerEl = document.querySelector(".container");
 
 var dayStart = 9;
-var dayEnd = 17;
+var dayEnd = 23;
 
-console.log(dayStart);
-// console.log(dayStart.format("h:mm"));
-console.log(moment(dayStart,'HH').format('HH:mm'));
-console.log(moment(dayEnd,'HH').format('HH:mm'));
+console.log(moment().format('HH:mm'));
 
+for (i = dayStart; i <= dayEnd; i++) {
+    // Create a new '<div>' for each hour in the business day
+    var timeContainer = $('<div>');
+    timeContainer.addClass('row');
+    containerEl.append(timeContainer);
 
-console.log(dayEnd-dayStart);
+    // Creates <div> that holds the time of the business day
+    var timeOfDayEl = $('<div>');
+    timeOfDayEl.addClass('hour col-1');
+    timeOfDayEl.text(moment(i, 'HH').format('HH:mm'));
+    timeContainer.append(timeOfDayEl);
 
-for(i = dayStart; i >= dayEnd; i++) {
-    // Create a new '<div>' for each hours in the business day
-    var timeslotEl = $('<div>');
-    
+    // Creates <div> that holds the detail about the activities for this time slot
+    var timeSlotDetail = $('<div>');
+    timeSlotDetail.addClass('col-10');
+    timeContainer.append(timeSlotDetail);
+
+    // if (i.toString() < moment().format('HH')) {
+    //     timeSlotDetail.addClass('past');
+    // } else if (i.toString() == moment().format('HH')) {
+    //     timeSlotDetail.addClass('present');
+    // } else timeSlotDetail.addClass('future');
+
+    if (moment(i.toString(), 'HH').format('HH') < moment().format('HH')) {
+        timeSlotDetail.addClass('past');
+    } else if (moment(i.toString(), 'HH').format('HH') == moment().format('HH')) {
+        timeSlotDetail.addClass('present');
+    } else timeSlotDetail.addClass('future');
+
+    // Creates <div> that holds the lock element
+    var timeSlotLock = $('<div>');
+    timeSlotLock.addClass('col-1 saveBtn');
+    timeContainer.append(timeSlotLock);
+
+    var lockEl = $('<i class="fa-solid fa-lock"></i>');
+    timeSlotLock.append(lockEl);
+
 }
+
+
+
+
+
+
+// var test = 21;
+
+
+// if (test.toString() < moment().format('HH')) {
+//     console.log('past');
+// } else if (test.toString() == moment().format('HH')) {
+//     console.log('present');
+// } else console.log('future');
+
+// if (moment(test.toString(), 'HH').format('HH') < moment().format('HH')) {
+//     console.log('past');
+// } else if (moment(test.toString(), 'HH').format('HH') == moment().format('HH')) {
+//     console.log('present');
+// } else console.log('future');
+
+// if (moment(test, 'HH').format('HH') < moment().format('HH')) {
+//     console.log('past');
+// } else if (moment(test, 'HH').format('HH') = moment().format('HH')) {
+//     console.log('present');
+// } else console.log('future');
+
