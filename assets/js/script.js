c427c29623d5c527b6ba7b591167aba7cad1e8e0
ionb23 <ion.borziac@gmail.com>
2023-01-23 23:35:28 +0000

    adding working code that saves input to local storage and displays it when page is refreshed

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var containerEl = $('#containerEl');
 // var containerEl = document.querySelector(".container");
 
 var dayStart = 9;
-var dayEnd = 23;
+var dayEnd = 17;
 
 console.log(moment().format('HH:mm'));
 
@@ -22,16 +22,12 @@ for (i = dayStart; i <= dayEnd; i++) {
     timeContainer.append(timeOfDayEl);
 
     // Creates <div> that holds the detail about the activities for this time slot
-    var timeSlotDetail = $('<div>');
+    var timeSlotDetail = $('<input>');
     timeSlotDetail.addClass('col-10');
+    timeSlotDetail.attr("id", "timeSlot" + i);
     timeContainer.append(timeSlotDetail);
 
-    // if (i.toString() < moment().format('HH')) {
-    //     timeSlotDetail.addClass('past');
-    // } else if (i.toString() == moment().format('HH')) {
-    //     timeSlotDetail.addClass('present');
-    // } else timeSlotDetail.addClass('future');
-
+    // Changes color of time slot based on current time of day
     if (moment(i.toString(), 'HH').format('HH') < moment().format('HH')) {
         timeSlotDetail.addClass('past');
     } else if (moment(i.toString(), 'HH').format('HH') == moment().format('HH')) {
@@ -41,36 +37,64 @@ for (i = dayStart; i <= dayEnd; i++) {
     // Creates <div> that holds the lock element
     var timeSlotLock = $('<div>');
     timeSlotLock.addClass('col-1 saveBtn');
+
     timeContainer.append(timeSlotLock);
 
     var lockEl = $('<i class="fa-solid fa-lock"></i>');
+
     timeSlotLock.append(lockEl);
+    // var storedEvent = $('timeSlot'+i);
+
+    // timeSlotLock.on('click', function () {
+    //     // localStorage.setItem("timeSlot"+i, JSON.stringify(storedScore));
+    //     localStorage.setItem("timeSlot"+i, storedEvent);
+    //     // if (isDark) {
+    //     //   $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+    //     //   isDark = !isDark;
+    //     // } else {
+    //     //   $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
+    //     //   isDark = !isDark;
+    //     // }
+    //   });
 
 }
 
+// ------------------
+
+containerEl.children().on("click", ".saveBtn", function () {
+
+    var selectedEl = $($(this).parent().children().eq(1));
+    var selectedElID = selectedEl.attr('id');
+    var enteredText = document.getElementById(selectedElID).value.trim();
+
+    console.log(selectedEl);
+    console.log(selectedElID);
+    console.log(enteredText);
+
+    localStorage.setItem(selectedElID, enteredText);
+
+    document.getElementById(selectedElID).value = localStorage.getItem(selectedElID);
 
 
+    // localStorage.setItem("timeSlot"+i, JSON.stringify(storedScore));
+    // localStorage.setItem(selectedEl, selectedEl);
+    // if (isDark) {
+    //   $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+    //   isDark = !isDark;
+    // } else {
+    //   $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
+    //   isDark = !isDark;
+    // }
+});
 
+document.getElementById('timeSlot9').value = localStorage.getItem('timeSlot9');
+document.getElementById('timeSlot10').value = localStorage.getItem('timeSlot10');
+document.getElementById('timeSlot11').value = localStorage.getItem('timeSlot11');
+document.getElementById('timeSlot12').value = localStorage.getItem('timeSlot12');
+document.getElementById('timeSlot13').value = localStorage.getItem('timeSlot13');
+document.getElementById('timeSlot14').value = localStorage.getItem('timeSlot14');
+document.getElementById('timeSlot15').value = localStorage.getItem('timeSlot15');
+document.getElementById('timeSlot16').value = localStorage.getItem('timeSlot16');
+document.getElementById('timeSlot17').value = localStorage.getItem('timeSlot17');
 
-
-// var test = 21;
-
-
-// if (test.toString() < moment().format('HH')) {
-//     console.log('past');
-// } else if (test.toString() == moment().format('HH')) {
-//     console.log('present');
-// } else console.log('future');
-
-// if (moment(test.toString(), 'HH').format('HH') < moment().format('HH')) {
-//     console.log('past');
-// } else if (moment(test.toString(), 'HH').format('HH') == moment().format('HH')) {
-//     console.log('present');
-// } else console.log('future');
-
-// if (moment(test, 'HH').format('HH') < moment().format('HH')) {
-//     console.log('past');
-// } else if (moment(test, 'HH').format('HH') = moment().format('HH')) {
-//     console.log('present');
-// } else console.log('future');
 
